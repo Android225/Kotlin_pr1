734830d3a19b02acc2c7a7adb52674f79976cab2
Andry <a.krishtofor@gmail.com>
2023-10-17 07:29:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_pr1/Fragment1.kt
@@ -0,0 +1,41 @@
+package com.example.kotlin_pr1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.fragment.findNavController
+
+class Fragment1 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment1, container, false)
+
+        // Переход к Фрагменту 2 с помощью
+        // ручного управление транзакциями фрагментов
+        val buttonToFragment2 = view.findViewById<Button>(R.id.button_to_fragment2)
+
+        buttonToFragment2.setOnClickListener {
+            val fragment2 = Fragment2()
+            val fragmentTransaction = activity?.supportFragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragment_container,fragment2)
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            fragmentTransaction?.commit()
+        }
+
+        // Переход к Фрагменту 3 с помощью Navigation API
+        val buttonToFragment3 = view.findViewById<Button>(R.id.button_to_fragment3)
+
+        buttonToFragment3.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment1_to_fragment3)
+        }
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/kotlin_pr1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin_pr1
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_container)
+    }
+}
